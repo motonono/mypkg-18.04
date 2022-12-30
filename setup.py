9571cd74fce02982fbdc1cd71d93876ecfed1cab
Soma <s21C1093JG@s.chibakoudai.jp>
2022-12-31 08:59:18 +0900

    talker

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*.launch.py'))
-    ],
+        ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='nomoto',
